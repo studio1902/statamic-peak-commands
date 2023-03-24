0816a4a0bb8006e46681495bc743bfb30cf594f3
Rob de Kort <rob@studio1902.nl>
2023-03-24 13:44:33 +0100

    Add image credits preset

--- src/Commands/InstallPresetPresets.php
+++ src/Commands/InstallPresetPresets.php
@@ -253,6 +253,36 @@ trait InstallPresetPresets {
                 ],
             ],
             [
+                'handle' => 'image_credits',
+                'name' => 'Image credits',
+                'description' => 'List images and their credits when copyright information has been added to an asset.',
+                'operations' => [
+                    [
+                        'type' => 'copy',
+                        'input' => 'image_credits.antlers.html.stub',
+                        'output' => 'resources/views/page_builder/_image_credits.antlers.html'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'image_credits_fieldset.yaml.stub',
+                        'output' => 'resources/fieldsets/image_credits.yaml'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'images_blueprint.yaml.stub',
+                        'output' => 'resources/blueprints/assets/images.yaml'
+                    ],
+                    [
+                        'type' => 'update_page_builder',
+                        'block' => [
+                            'name' => 'Image credits',
+                            'instructions' => 'List images with their credits.',
+                            'handle' => 'image_credits',
+                        ]
+                    ],
+                ]
+            ],
+            [
                 'handle' => 'language_picker',
                 'name' => 'Language picker',
                 'description' => 'A language picker for when you use multisite.',
