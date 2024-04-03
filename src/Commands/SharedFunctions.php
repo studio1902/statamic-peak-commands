ab25aafa48668219a4dcc5bde3454e84ecffd332
Rob de Kort <rob@studio1902.nl>
2024-04-03 13:17:52 +0200

    Add the ability to create new article groups

--- src/Commands/SharedFunctions.php
+++ src/Commands/SharedFunctions.php
@@ -147,22 +147,67 @@ trait SharedFunctions {
         ];
 
         $existingGroups = Arr::get($fieldset, 'fields.0.field.sets');
-        $group = select(
-            label: "In which group of article sets do you want to install: '{$name}'?",
-            options: array_keys($existingGroups),
-            scroll: 10
+
+        $useExistingGroup = confirm(
+            label: 'Do you want to add this set to an existing or new group?',
+            yes: 'Existing',
+            no: 'New',
+            default: true
         );
 
-        $groupSets = $existingGroups[$group];
-        $existingSets = Arr::get($groupSets, 'sets');
-        $existingSets[$filename] = $newSet;
-        $existingSets = collect($existingSets)->sortBy(function ($value, $key) {
-            return $key;
-        })->all();
+        if ($useExistingGroup) {
 
-        Arr::set($groupSets, 'sets', $existingSets);
-        $existingGroups[$group] = $groupSets;
-        Arr::set($fieldset, 'fields.0.field.sets', $existingGroups);
+            $group = select(
+                label: "In which group of article sets do you want to install: '{$name}'?",
+                options: array_keys($existingGroups),
+                scroll: 10
+            );
+
+            $groupSets = $existingGroups[$group];
+            $existingSets = Arr::get($groupSets, 'sets');
+            $existingSets[$filename] = $newSet;
+            $existingSets = collect($existingSets)->sortBy(function ($value, $key) {
+                return $key;
+            })->all();
+
+            Arr::set($groupSets, 'sets', $existingSets);
+            $existingGroups[$group] = $groupSets;
+            Arr::set($fieldset, 'fields.0.field.sets', $existingGroups);
+
+        } else {
+            $groupName = text(
+                label: 'What should be the name for this group?',
+                placeholder: 'Collections',
+                required: true
+            );
+
+            $groupFilename = Stringy::slugify($groupName, '_');
+
+            $groupInstructions = text(
+                label: 'What should be the instructions for this group?',
+                placeholder: 'Collection based content.',
+                required: true
+            );
+
+            $groupIcon = $this->promptsIconPicker('Which icon do you want to use for this group?');
+
+            $newGroup = [
+                $groupFilename => [
+                    'display' => $groupName,
+                    'instructions' => $groupInstructions,
+                    'icon' => $groupIcon,
+                    'sets' => [
+                        $newSet
+                    ]
+                ]
+            ];
+
+            $groups = array_merge($existingGroups, $newGroup);
+            $orderedGroups = collect($groups)->sortBy(function ($key) {
+                return $key;
+            })->all();
+            Arr::set($fieldset, 'fields.0.field.sets', $orderedGroups);
+        }
 
         File::put(base_path('resources/fieldsets/article.yaml'), Yaml::dump($fieldset, 99, 2));
     }
