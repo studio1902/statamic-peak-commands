aa5566d76b5330a3ee677a770a0dd5fddede55bd
Rob de Kort <rob@studio1902.nl>
2025-01-22 13:36:15 +0100

    Add mega menu preset

--- src/Commands/InstallPresetPresets.php
+++ src/Commands/InstallPresetPresets.php
@@ -432,6 +432,41 @@ trait InstallPresetPresets {
                 ]
             ],
             [
+                'handle' => 'mega_menu',
+                'name' => 'Mega Menu',
+                'description' => 'A three level deep mega dropdown menu.',
+                'operations' => [
+                    [
+                        'type' => 'notify',
+                        'content' => "This preset assumes you still have a navigation called `main`, separated into the two default Peak templates: mobile and desktop."
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'content/navigation/main.yaml.stub',
+                        'output' => 'content/navigation/main.yaml'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'resources/blueprints/navigation/main.yaml.stub',
+                        'output' => 'resources/blueprints/navigation/main.yaml'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'resources/views/navigation/_main_desktop.antlers.html.stub',
+                        'output' => 'resources/views/navigation/_main_desktop.antlers.html'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'resources/views/navigation/_main_mobile.antlers.html.stub',
+                        'output' => 'resources/views/navigation/_main_mobile.antlers.html'
+                    ],
+                    [
+                        'type' => 'notify',
+                        'content' => "Make sure to add items, section headers and pages with descriptions to your navigation first."
+                    ],
+                ]
+            ],
+            [
                 'handle' => 'news',
                 'name' => 'News',
                 'singular_name' => 'Item',
