496f3a8c9adb34a541901e663ccd500afd1211a4
Marco Rieser <marco@rieser.dev>
2025-02-04 15:48:58 +0100

    Add notify and update_role operations

---
+++ src/Operations/Notify.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Studio1902\PeakCommands\Operations;
+
+use Illuminate\Support\Str;
+use Studio1902\PeakCommands\Models\Installable;
+use function Laravel\Prompts\pause;
+use function Laravel\Prompts\warning;
+
+class Notify extends Operation
+{
+    protected string $content = '';
+
+    public function __construct(array $config)
+    {
+        $this->content = $config['content'];
+    }
+
+    public function run(): Installable
+    {
+        $message = (string)Str::of($this->content)
+            ->replace('{{ handle }}', $this->installable->renameHandle)
+            ->replace('{{ name }}', $this->installable->renameName);
+
+        warning("\n" . $message . "\n");
+
+        pause('Follow the instructions and press ENTER to continue.');
+
+        return $this->installable;
+    }
+}
---
+++ src/Operations/UpdateRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Studio1902\PeakCommands\Operations;
+
+use Illuminate\Support\Facades\File;
+use Statamic\Support\Arr;
+use Studio1902\PeakCommands\Models\Installable;
+use Symfony\Component\Yaml\Yaml;
+
+class UpdateRole extends Operation
+{
+    protected string $role = '';
+    protected array $permissions = [];
+
+    public function __construct(array $config)
+    {
+        $this->role = $config['role'];
+        $this->permissions = $config['permissions'];
+    }
+
+    public function run(): Installable
+    {
+        $roles = Yaml::parseFile(base_path('resources/users/roles.yaml'));
+
+        $existingPermissions = Arr::get($roles, "$this->role.permissions");
+        $permissions = array_merge($existingPermissions, str_replace('{{ handle }}', $this->installable->renameHandle, $this->permissions));
+
+        Arr::set($roles, 'editor.permissions', $permissions);
+
+        File::put(base_path('resources/users/roles.yaml'), Yaml::dump($roles, 99, 2));
+
+        return $this->installable;
+    }
+}
