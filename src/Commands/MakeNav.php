c9e35038877be60ffc20da63a5fd9961ff204912
Marco Rieser <marco@rieser.dev>
2025-03-31 12:02:54 +0200

    Restructure MakeNav command

--- src/Commands/MakeNav.php
+++ src/Commands/MakeNav.php
@@ -3,17 +3,11 @@
 namespace Studio1902\PeakCommands\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
 use Statamic\Console\RunsInPlease;
-use Statamic\Facades\Collection;
-use Statamic\Support\Arr;
 use Studio1902\PeakCommands\Commands\Traits\CanClearCache;
 use Studio1902\PeakCommands\Commands\Traits\NeedsValidLicense;
-use Symfony\Component\Yaml\Yaml;
-use function Laravel\Prompts\confirm;
-use function Laravel\Prompts\multisearch;
-use function Laravel\Prompts\text;
+use Studio1902\PeakCommands\Models\Installable;
+use Studio1902\PeakCommands\Models\Nav;
 
 class MakeNav extends Command
 {
@@ -21,126 +15,89 @@ class MakeNav extends Command
 
     protected $name = 'statamic:peak:make:nav';
     protected $description = "Make a navigation.";
-    protected $navigation_name = '';
-    protected $filename = '';
-    protected $max_depth = 1;
-    protected $collections = [];
-    protected $permissions = true;
+
+    protected array $operations = [];
+    protected ?Nav $model = null;
 
     public function handle()
     {
         $this->checkLicense();
 
-        $this->navigation_name = text(
-            label: 'What should be the name for this navigation?',
-            placeholder: 'E.g. Actions',
-            required: true
-        );
+        $this->createModel();
+        $this->createConfiguration();
+        $this->createBlueprint();
+        $this->attachCollections();
+        $this->grantPermissions();
 
-        $this->filename = Str::slug($this->navigation_name, '_');
-
-        $this->max_depth = text(
-            label: 'What should be the max depth for this navigation?',
-            placeholder: '2',
-            validate: ['name' => 'required|int|numeric']
-        );
-
-        $options = collect(Collection::all())->pluck('title', 'handle');
-
-        $this->collections = multisearch(
-            label: "Enable linking to entries from these collections:",
-            options: fn(string $value) => strlen($value) > 0
-                ? $options->filter(fn(string $item) => Str::contains($item, $value, true))->toArray()
-                : $options->toArray(),
-            scroll: 15
-        );
-
-        $this->permissions = confirm(
-            label: 'Grant edit permissions to editor role?',
-            default: true
-        );
-
-        try {
-            $this->createNavigation();
-            $this->createBlueprint();
-            $this->attachCollections();
-            $this->handlePermissions();
-        } catch (\Exception $e) {
-            return $this->error($e->getMessage());
-        }
-
+        $this->runOperations();
         $this->clearCache();
 
-        $this->info("<info>[✓]</info> Navigation '{$this->navigation_name}' created.");
+        $this->info("<info>[✓]</info> Navigation '{$this->model->name}' created.");
     }
 
-    /**
-     * Create fieldset.
-     *
-     * @return bool|null
-     */
-    protected function createNavigation()
+    protected function createConfiguration(): void
     {
-        $this->checkExistence('Navigation', "content/navigation/{$this->filename}.yaml");
-
-        $stub = $this->getStub('/navigation.yaml.stub');
-        $contents = Str::of($stub)
-            ->replace('{{ navigation_name }}', $this->navigation_name)
-            ->replace('{{ max_depth }}', $this->max_depth);
-
-        File::put(base_path("content/navigation/{$this->filename}.yaml"), $contents);
-    }
-
-    /**
-     * Create blueprints.
-     *
-     * @return bool|null
-     */
-    protected function createBlueprint()
-    {
-        $this->checkExistence('Blueprint', "resources/blueprints/navigation/{$this->filename}.yaml");
-
-        $contents = Str::of(Str::of($this->getStub('/navigation_blueprint.yaml.stub')));
-
-        if (!File::exists("resources/blueprints/navigation")) File::makeDirectory("resources/blueprints/navigation");
-
-        File::put(base_path("resources/blueprints/navigation/{$this->filename}.yaml"), $contents);
-    }
-
-    /**
-     * Attach Collections.
-     *
-     * @return bool|null
-     */
-    protected function attachCollections()
-    {
-        if (!$this->collections) {
-            return;
-        }
-
-        $nav = Yaml::parseFile(base_path("content/navigation/{$this->filename}.yaml"));
-
-        Arr::set($nav, 'collections', $this->collections);
-
-        File::put(base_path("content/navigation/{$this->filename}.yaml"), Yaml::dump($nav, 99, 2));
-    }
-
-    /**
-     * Handle permissions
-     *
-     * @return bool|null
-     */
-    protected function handlePermissions()
-    {
-        if (!$this->permissions) {
-            return;
-        }
-
-        $permissions = [
-            "view {$this->filename} nav",
-            "edit {$this->filename} nav",
+        $this->operations[] = [
+            'type' => 'copy',
+            'input' => 'navigation.yaml.stub',
+            'output' => "content/navigation/{{ handle }}.yaml",
+            'replacements' => [
+                '{{ navigation_name }}' => $this->model->name,
+                '{{ max_depth }}' => $this->model->maxDepth,
+            ]
         ];
+    }
 
-        $this->grantPermissionsToEditor($permissions);
+    protected function createBlueprint(): void
+    {
+        $this->operations[] = [
+            'type' => 'copy',
+            'input' => 'navigation_blueprint.yaml.stub',
+            'output' => "resources/blueprints/navigation/{{ handle }}.yaml"
+        ];
+    }
+
+    protected function attachCollections(): void
+    {
+        $this->operations[] = [
+            'type' => 'attach_collections_to_navigation',
+            'navigation' => $this->model->filename,
+            'collections' => $this->model->collections,
+        ];
+    }
+
+    protected function createModel(): void
+    {
+        $this->model = app()->make(Nav::class);
+    }
+
+    protected function grantPermissions(): void
+    {
+        if (!$this->model->grantPermissions) {
+            return;
+        }
+
+        $this->operations[] = [
+            'type' => 'update_role',
+            'role' => 'editor',
+            'permissions' => [
+                "view {{ handle }} nav",
+                "edit {{ handle }} nav",
+            ],
+        ];
+    }
+
+    protected function runOperations(): void
+    {
+        app()
+            ->make(Installable::class, [
+                'config' => [
+                    'name' => $this->model->name,
+                    'handle' => $this->model->filename,
+                    'operations' => $this->operations,
+                    'path' => base_path('vendor/studio1902/statamic-peak-commands/resources/stubs'),
+                ]
+            ])
+            ->install();
     }
 }
---
+++ src/Models/Nav.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Studio1902\PeakCommands\Models;
+
+use Illuminate\Support\Str;
+use Statamic\Facades\Collection;
+use Statamic\Facades\Config;
+use Statamic\Support\Arr;
+use Stringy\StaticStringy as Stringy;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\multisearch;
+use function Laravel\Prompts\text;
+
+class Nav
+{
+    public string $name;
+    public string $filename;
+    public int $maxDepth;
+    public array $collections;
+    public bool $grantPermissions;
+
+    public function __construct(array $config = [])
+    {
+        $this->name = Arr::get($config, 'name') ?? $this->promptForName();
+        $this->maxDepth = Arr::get($config, 'max_depth') ?? $this->promptForMaxDepth();
+        $this->collections = Arr::get($config, 'collections') ?? $this->promptForCollections();
+        $this->grantPermissions = Arr::get($config, 'permissions') ?? $this->promptForPermissions();
+        $this->filename = $this->generateFilename();
+    }
+
+    protected function promptForName(): string
+    {
+        return text(
+            label: 'What should be the name for this navigation?',
+            placeholder: 'E.g. Actions',
+            required: true
+        );
+    }
+
+    protected function generateFilename(): string
+    {
+        return Stringy::slugify($this->name, '_', Config::getShortLocale());
+    }
+
+    protected function promptForCollections(): array
+    {
+        $options = collect(Collection::all())->pluck('title', 'handle');
+
+        return multisearch(
+            label: "Enable linking to entries from these collections:",
+            options: fn(string $value) => strlen($value) > 0
+                ? $options->filter(fn(string $item) => Str::contains($item, $value, true))->toArray()
+                : $options->toArray(),
+            scroll: 15
+        );
+    }
+
+    protected function promptForPermissions(): bool
+    {
+        return confirm(
+            label: 'Grant edit permissions to editor role?',
+            default: true
+        );
+    }
+
+    protected function promptForMaxDepth(): int
+    {
+        return (int)text(
+            label: 'What should be the max depth for this navigation?',
+            placeholder: '2',
+            validate: ['name' => 'required|int|numeric']
+        );
+    }
+}
