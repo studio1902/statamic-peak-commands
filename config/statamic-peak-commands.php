0d4dc3bdb15f73e8a891fe40a83f29833949d06a
Marco Rieser <marco@rieser.dev>
2025-05-06 11:40:24 +0200

    Add comments to config file

--- config/statamic-peak-commands.php
+++ config/statamic-peak-commands.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'namespaces' => [
-        '\Studio1902\PeakCommands\Operations',
-    ],
+    /**
+     * Here you can configure the paths in which the commands addon
+     * searches for premade blocks, presets and article sets.
+     */
     'paths' => [
         'blocks' => [
             base_path('vendor/studio1902/statamic-peak-commands/resources/blocks'),
@@ -14,5 +15,13 @@ return [
         'sets' => [
             base_path('vendor/studio1902/statamic-peak-commands/resources/sets'),
         ],
+    ],
+
+    /**
+     * Here you can configure the namespaces in which the commands addon
+     * searches for available operations to run.
+     */
+    'namespaces' => [
+        '\Studio1902\PeakCommands\Operations',
     ],
 ];
