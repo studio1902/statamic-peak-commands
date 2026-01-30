5ae212551082937b6d63a7c6d78f5f2d73e501ab
Rob de Kort <rob@studio1902.nl>
2026-01-30 14:14:41 +0100

    Fix view transitions preset
    Closes #44

--- resources/presets/view_transitions/config.php
+++ resources/presets/view_transitions/config.php
@@ -11,6 +11,11 @@ return [
             'output' => 'resources/css/view-transitions.css',
         ],
         [
+            'type' => 'copy',
+            'input' => 'resources/views/snippets/_speculation_rules.antlers.html',
+            'output' => 'resources/views/snippets/_speculation_rules.antlers.html',
+        ],
+        [
             'type' => 'notify',
             'content' => "Add `\n@import \"./view-transitions.css\";` to the top of your `resources/css/site.css` file.",
         ],
